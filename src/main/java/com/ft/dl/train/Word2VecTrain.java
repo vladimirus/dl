eafc68097d52594cd40d2700ab2a37502b561125
Parshikov, Vladimir <vladimir.parshikov@ft.com>
2016-11-14 17:12:21 +0000

    wip

---
+++ src/main/java/com/ft/dl/train/Word2VecTrain.java
@@ -0,0 +1,100 @@
+package com.ft.dl.train;
+
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.tuple.Pair;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.text.sentenceiterator.LineSentenceIterator;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+public class Word2VecTrain {
+
+    public static final String MODEL = "/Users/vladimir/Downloads/all-body-model.txt";
+
+    public static void main(String[] args) {
+
+        Word2VecTrain word2Vec = new Word2VecTrain();
+
+        WordVectors vec;
+        if (Files.exists(Paths.get(MODEL))) {
+            vec = loadModel(Paths.get(MODEL).toFile());
+        } else {
+            vec = word2Vec.train(Paths.get("/Users/vladimir/Downloads/all-body.txt"));
+        }
+
+        word2Vec.listen(p -> System.out.println(vec.wordsNearest(p.getLeft(), p.getRight(), 10)));
+    }
+
+    @SneakyThrows
+    private static WordVectors loadModel(File file) {
+        System.out.println("Loading vectors from a file");
+        return WordVectorSerializer.loadTxtVectors(file);
+    }
+
+    private void listen(Consumer<Pair<Collection<String>, Collection<String>>> callback) {
+        System.out.println("Ready");
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            Collection<String> positive = new ArrayList<>();
+            Collection<String> negative = new ArrayList<>();
+            Collection<String> current = positive;
+            String[] array = scanner.nextLine().split("(?=[-+])|(?<=[-+])");
+            for (String str : array) {
+                str = str.trim();
+                switch (str) {
+                    case "+":
+                        current = positive;
+                        break;
+                    case "-":
+                        current = negative;
+                        break;
+                    default:
+                        current.add(str);
+                        break;
+                }
+            }
+            callback.accept(Pair.of(positive, negative));
+        }
+    }
+
+    @SneakyThrows
+    private Word2Vec train(Path path) {
+        SentenceIterator it = new LineSentenceIterator(path.toFile());
+        it.setPreProcessor(String::toLowerCase);
+
+        TokenizerFactory t = new DefaultTokenizerFactory();
+        t.setTokenPreProcessor(new CommonPreprocessor());
+
+        Word2Vec vec = new Word2Vec.Builder()
+                .minWordFrequency(5)
+                .iterations(1)
+                .layerSize(100)
+                .seed(42)
+                .windowSize(5)
+                .iterate(it)
+                .tokenizerFactory(t)
+                .build();
+
+        vec.fit();
+
+        System.out.println(vec.wordsNearest("putin", 10));
+
+        WordVectorSerializer.writeWordVectors(vec, MODEL);
+//
+        return vec;
+    }
+}
--- src/main/java/com/ft/dl/transform/OneFileWithBody.java
+++ src/main/java/com/ft/dl/transform/OneFileWithBody.java
@@ -1,5 +1,8 @@
 package com.ft.dl.transform;
 
+import static org.jsoup.Jsoup.parseBodyFragment;
+import static rx.Observable.from;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ft.dl.converter.ContentConverter;
 import com.ft.dl.service.FileManager;
@@ -12,9 +15,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
 
-import static org.jsoup.Jsoup.parseBodyFragment;
-import static rx.Observable.from;
-
 public class OneFileWithBody {
     private FileManager fileManager;
     private ContentConverter contentConverter;
@@ -25,13 +25,13 @@ public class OneFileWithBody {
     }
 
     public static void main(String[] args) {
-        FileManager fileManager = new FileManager(Paths.get("/media/vov/Untitled/content"));
+        FileManager fileManager = new FileManager(Paths.get("/Volumes/Untitled/content"));
         ContentConverter contentConverter = new ContentConverter(new ObjectMapper());
         new OneFileWithBody(fileManager, contentConverter).transform();
     }
 
     public void transform() {
-        Path file = Paths.get("/home/vov/Downloads/all-body.txt");
+        Path file = Paths.get("/Users/vladimir/Downloads/all-body.txt");
         from(() -> fileManager.walk().iterator())
                 .filter(p -> !Files.isDirectory(p))
                 .map(p -> contentConverter.enrichedContent(fileManager.read(p)))
